1e8fc3521daa11a2907ed6700a8166cf727306f6
CharaeKeow <charaekeow11@gmail.com>
2025-10-26 19:47:00 +0800

    chore(web): cleanup create tanstack app boostrap code

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -6,7 +6,6 @@ import {
 } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 
-import Header from '../components/Header';
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 import appCss from '../styles.css?url';
 
@@ -27,7 +26,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Job Application Tracker',
       },
     ],
     links: [
@@ -48,7 +47,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <Header />
         {children}
         <TanStackDevtools
           config={{
